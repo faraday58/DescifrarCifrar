af17fa6befd2aae5b6747f4c59251b05e2c2c367
Armando Rodríguez Vera <jarodriguezvera@gmail.com>
2018-05-22 23:00:38 -0500

    En el Form1 está todo el código, primero debes de abrir el archivo y después elegir cifrarlo o descifrarlo dependiendo el caso

--- DescifrarCifrar/Form1.Designer.cs
+++ DescifrarCifrar/Form1.Designer.cs
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cifradoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cifrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.descifrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.cifradoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(771, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // cifradoToolStripMenuItem
+            // 
+            this.cifradoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cifrarToolStripMenuItem,
+            this.descifrarToolStripMenuItem});
+            this.cifradoToolStripMenuItem.Name = "cifradoToolStripMenuItem";
+            this.cifradoToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.cifradoToolStripMenuItem.Text = "Cifrado";
+            // 
+            // cifrarToolStripMenuItem
+            // 
+            this.cifrarToolStripMenuItem.Name = "cifrarToolStripMenuItem";
+            this.cifrarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cifrarToolStripMenuItem.Text = "Cifrar";
+            this.cifrarToolStripMenuItem.Click += new System.EventHandler(this.cifrarToolStripMenuItem_Click);
+            // 
+            // descifrarToolStripMenuItem
+            // 
+            this.descifrarToolStripMenuItem.Name = "descifrarToolStripMenuItem";
+            this.descifrarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.descifrarToolStripMenuItem.Text = "Descifrar";
+            this.descifrarToolStripMenuItem.Click += new System.EventHandler(this.descifrarToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(771, 305);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cifradoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cifrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem descifrarToolStripMenuItem;
     }
 }
 
--- DescifrarCifrar/Form1.cs
+++ DescifrarCifrar/Form1.cs
@@ -6,15 +6,123 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace DescifrarCifrar
 {
     public partial class Form1 : Form
     {
+        FileStream fs;
+        DESCryptoServiceProvider cryptic;
+        CryptoStream crStream;
+        byte[] bytes;
         public Form1()
         {
             InitializeComponent();
         }
+
+        
+        
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK )
+            {
+                fs = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.ReadWrite);
+                bytes = new byte[fs.Length];           
+                
+
+            }
+            MessageBox.Show("Archivo abierto");
+
+
+        }
+
+        private void cifrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cryptic = new DESCryptoServiceProvider();
+                cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+                cryptic.IV = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+
+                int numBytesToRead = (int)fs.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = fs.Read(bytes, numBytesRead, numBytesToRead);
+
+                    if (n == 0)
+                        break;
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+
+                }
+
+                crStream = new CryptoStream(fs, cryptic.CreateEncryptor(), CryptoStreamMode.Write);
+                fs.Seek(0, SeekOrigin.Begin);
+                crStream.Write(bytes,0, (int)fs.Length);
+
+
+            }
+            catch (IOException error)
+            {
+                MessageBox.Show("Error " + error.Message);
+            }
+            finally
+            {
+                crStream.Close();
+                fs.Close();
+            }
+
+            MessageBox.Show("Proceso Terminado");
+
+        }
+
+        private void descifrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cryptic = new DESCryptoServiceProvider();
+                cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+                cryptic.IV = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");
+
+                int numBytesToRead = (int)fs.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = fs.Read(bytes, numBytesRead, numBytesToRead);
+
+                    if (n == 0)
+                        break;
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+
+                }
+
+                crStream = new CryptoStream(fs, cryptic.CreateDecryptor(), CryptoStreamMode.Write);
+                fs.Seek(0, SeekOrigin.Begin);
+                crStream.Write(bytes, 0, (int)fs.Length);
+
+
+            }
+            catch (IOException error)
+            {
+                MessageBox.Show("Error " + error.Message);
+            }
+            finally
+            {
+                crStream.Close();
+                fs.Close();
+            }
+
+            MessageBox.Show("Se ha descifrado el archivo");
+
+        }
     }
 }
+ 
